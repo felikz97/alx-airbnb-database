4eee8b107d519957df51298a71d3c5b92c058949
Felix Kipkemoi <felikz.kipkemoi@gmail.com>
2025-05-14 16:21:43 +0300

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -28,3 +28,16 @@ WHERE
         FROM bookings AS b
         WHERE b.user_id = u.id          -- correlated reference to the outer query
     ) > 3;
+-- query to find the total number of bookings made by each user, using the COUNT function and GROUP BY clause
+SELECT 
+    u.id AS user_id,
+    u.name AS user_name,
+    COUNT(b.id) AS total_bookings
+FROM 
+    users u
+LEFT JOIN 
+    bookings b ON u.id = b.user_id
+GROUP BY 
+    u.id, u.name
+ORDER BY 
+    total_bookings DESC;
