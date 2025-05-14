e0509069effb1981aec24ce8b3e7813811c2cabb
Felix Kipkemoi <felikz.kipkemoi@gmail.com>
2025-05-14 17:16:02 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,1 +1,14 @@
+EXPLAIN
+SELECT 
+    u.id,
+    u.name,
+    b.id AS booking_id,
+    b.booking_date
+FROM 
+    users u
+JOIN 
+    bookings b ON u.id = b.user_id
+WHERE 
+    b.status = 'confirmed'
+    AND b.booking_date >= '2024-01-01';
 
