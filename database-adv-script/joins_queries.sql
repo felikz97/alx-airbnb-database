4f0d8841deb2816ad10d8dd80b7f7a101519f7b6
Felix Kipkemoi <felikz.kipkemoi@gmail.com>
2025-05-14 16:11:16 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -10,6 +10,7 @@ FROM
 INNER JOIN 
     users ON bookings.user_id = users.id;
 
+
 SELECT 
     properties.id AS property_id,
     properties.name AS property_name,
@@ -21,3 +22,16 @@ FROM
     properties
 LEFT JOIN 
     reviews ON properties.id = reviews.property_id;
+
+
+SELECT 
+    users.id AS user_id,
+    users.name AS user_name,
+    users.email,
+    bookings.id AS booking_id,
+    bookings.booking_date,
+    bookings.status
+FROM 
+    users
+FULL OUTER JOIN 
+    bookings ON users.id = bookings.user_id;
