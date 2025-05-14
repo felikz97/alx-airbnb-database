97740df5d87be240b0febfd04de26af9ce7d65e9
Felix Kipkemoi <felikz.kipkemoi@gmail.com>
2025-05-14 16:17:42 +0300

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,18 @@
+SELECT 
+    p.id AS property_id,
+    p.name AS property_name,
+    p.location
+FROM 
+    properties p
+WHERE 
+    p.id IN (
+        SELECT 
+            r.property_id
+        FROM 
+            reviews r
+        GROUP BY 
+            r.property_id
+        HAVING 
+            AVG(r.rating) > 4.0
+    );
 
