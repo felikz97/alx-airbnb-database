5a9e74045c63aa6319c0f9762ecd4dc24d2c8ee0
Felix Kipkemoi <felikz.kipkemoi@gmail.com>
2025-05-14 16:07:33 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,12 @@
+SELECT 
+    bookings.id AS booking_id,
+    bookings.booking_date,
+    bookings.status,
+    users.id AS user_id,
+    users.name,
+    users.email
+FROM 
+    bookings
+INNER JOIN 
+    users ON bookings.user_id = users.id;
 
